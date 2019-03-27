003a971f2cb0d9630d67f86cb1a453cd542fcce1
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-27 10:41:07 +0100

    Setup ready

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId
+
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  favorites: [{
+    type: ObjectId,
+    ref: 'Guide'
+  }],
+  own: [{
+    type: ObjectId,
+    ref: 'Guide'
+  }]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
