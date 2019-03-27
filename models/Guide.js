a406bbf63504f787c773ab0ed10ec939209426bb
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-27 11:22:32 +0100

    Model Guide Done (initial)

---
+++ models/Guide.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId
+
+
+const guideSchema = new Schema({
+  creator: {
+    type: ObjectId,
+    required: true
+  },
+  places: [{
+    name: String,
+    description: String,
+    what: String,
+    location: String
+  }],
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const Guide = mongoose.model('Guide', guideSchema);
+
+module.exports = Guide;
